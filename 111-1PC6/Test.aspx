61695f75861b861c00732f7ecadf0c8fda37d054
109111114 <a0921567518@gmail.com>
2022-10-26 11:30:51 +0800

    草稿1

---
+++ 111-1PC6/Test.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="_111_1PC6.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="ddl_Test" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="dd1_Test" runat="server"></asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 111-1PC6/Test.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC6
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        string[] s_school = new string[3] { "台科", "北科", "亞東" };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int i_ct = 0; i_ct < s_school.Length; i_ct++)
+            {
+                ListItem L_I = new ListItem();
+                L_I.Text = s_school[i_ct];
+                L_I.Value = s_school[i_ct];
+
+                dd1_Test.Items.Add(L_I);
+            }
+            
+        }
+    }
+}
